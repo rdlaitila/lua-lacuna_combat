8d7e6052a00839e58c66262b55d3ec9b9926ecea
admin36 <github@reganlaitila.net>
2014-12-27 01:10:01 -0500

    fasdf

--- conf.lua
+++ conf.lua
@@ -7,6 +7,8 @@ for key, value in pairs(arg) do
     print(key, value)
 end
 
+print(IS_SERVER)
+
 function love.conf(t)
     t.identity = nil                   -- The name of the save directory (string)
     t.version = "0.9.1"                -- The LÖVE version this game was made for (string)
