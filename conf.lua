5e889ac3d987cf340aa10ee6b5422580a3466c3f
admin36 <github@reganlaitila.net>
2014-12-27 01:08:36 -0500

    fasdf

--- conf.lua
+++ conf.lua
@@ -11,22 +11,6 @@ function love.conf(t)
     t.identity = nil                   -- The name of the save directory (string)
     t.version = "0.9.1"                -- The LÖVE version this game was made for (string)
     t.console = false                  -- Attach a console (boolean, Windows only)
-
-    t.window.title = "Lacuna Combat"        -- The window title (string)
-    t.window.icon = nil                -- Filepath to an image to use as the window's icon (string)
-    t.window.width = 800               -- The window width (number)
-    t.window.height = 600              -- The window height (number)
-    t.window.borderless = false        -- Remove all border visuals from the window (boolean)
-    t.window.resizable = false         -- Let the window be user-resizable (boolean)
-    t.window.minwidth = 1              -- Minimum window width if the window is resizable (number)
-    t.window.minheight = 1             -- Minimum window height if the window is resizable (number)
-    t.window.fullscreen = false        -- Enable fullscreen (boolean)
-    t.window.fullscreentype = "normal" -- Standard fullscreen or desktop fullscreen mode (string)
-    t.window.vsync = true              -- Enable vertical sync (boolean)
-    t.window.fsaa = 0                  -- The number of samples to use with multi-sampled antialiasing (number)
-    t.window.display = 1               -- Index of the monitor to show the window in (number)
-    t.window.highdpi = false           -- Enable high-dpi mode for the window on a Retina display (boolean). Added in 0.9.1
-    t.window.srgb = false              -- Enable sRGB gamma correction when drawing to the screen (boolean). Added in 0.9.1
     
     t.modules.event = true             -- Enable the event module (boolean)    
     t.modules.image = true             -- Enable the image module (boolean)
@@ -45,5 +29,21 @@ function love.conf(t)
         t.modules.graphics = true          -- Enable the graphics module (boolean)
         t.modules.sound = true             -- Enable the sound module (boolean)
         t.modules.window = true            -- Enable the window module (boolean)
+        
+        t.window.title = "Lacuna Combat"        -- The window title (string)
+        t.window.icon = nil                -- Filepath to an image to use as the window's icon (string)
+        t.window.width = 800               -- The window width (number)
+        t.window.height = 600              -- The window height (number)
+        t.window.borderless = false        -- Remove all border visuals from the window (boolean)
+        t.window.resizable = false         -- Let the window be user-resizable (boolean)
+        t.window.minwidth = 1              -- Minimum window width if the window is resizable (number)
+        t.window.minheight = 1             -- Minimum window height if the window is resizable (number)
+        t.window.fullscreen = false        -- Enable fullscreen (boolean)
+        t.window.fullscreentype = "normal" -- Standard fullscreen or desktop fullscreen mode (string)
+        t.window.vsync = true              -- Enable vertical sync (boolean)
+        t.window.fsaa = 0                  -- The number of samples to use with multi-sampled antialiasing (number)
+        t.window.display = 1               -- Index of the monitor to show the window in (number)
+        t.window.highdpi = false           -- Enable high-dpi mode for the window on a Retina display (boolean). Added in 0.9.1
+        t.window.srgb = false              -- Enable sRGB gamma correction when drawing to the screen (boolean). Added in 0.9.1
     end
 end
